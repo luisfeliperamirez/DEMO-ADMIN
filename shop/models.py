caaef518ae54330a309b3e3323f0e9d7f8af2c85
Luis <luisfelipe.ramirez@inacapmail.cl>
2026-09-24 12:21:23 -0300

    Migraciones y usuario creados

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo_electronico = models.EmailField(unique=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Peliculas(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_estreno = models.DateField()
+    duracion = models.PositiveIntegerField()
+    genero = models.CharField(max_length=100)
+    director = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.titulo
