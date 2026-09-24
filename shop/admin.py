c52965b857c3038b628b860b7fa7bb59576773c9
Luis <luisfelipe.ramirez@inacapmail.cl>
2026-09-24 12:40:11 -0300

    Modelos y migraciones integradas a Django Admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pelicula
 
-# Register your models here.
+class PeliculasAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha_estreno', 'duracion', 'genero', 'director')
+    search_fields = ('titulo', 'genero', 'director')
+    list_filter = ('fecha_estreno', 'genero')
+
+admin.site.register(Pelicula, PeliculasAdmin)
